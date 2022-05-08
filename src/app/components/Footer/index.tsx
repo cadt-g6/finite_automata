aae577e8ef289f73cf8f9798d53e0c0ccc978734
Sopanha Kok <38602188+SopanhaKok@users.noreply.github.com>
2022-05-09 00:12:26 +0700

    fix: update footer design (#26)

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -1,48 +1,24 @@
 import * as React from 'react';
 import { Typography, styled } from '@mui/material';
-import Avatar from '@mui/material/Avatar';
+import { Link } from 'react-router-dom';
 
-import Darot from "app/assets/team-images/darot.jpeg";
-import Panha from "app/assets/team-images/panha.jpeg";
-import Thea from "app/assets/team-images/thea.jpeg";
-import Vatanak from "app/assets/team-images/vatanak.jpeg";
-import Sarak from "app/assets/team-images/vatanak.jpeg";
 
 
 const StyleFooter = styled(`div`)(() => ({
-  backgroundColor: "grey",
-  textAlign: "center",
-  alignItems: "center",
-  justifyContent: "center",
-  alignContent: "center",
-  bottom: 0,
-  margin: "0px 0px 0px 00px",
-  padding: "0px 30px 100px 0px",
-  width: "100%"
+  backgroundColor: "#192849",
+  display:'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
 }));
 
-const AvatarStyle = {
-  width: 48,
-  height: 48,
-  margin: "0px 4px",
-  borderStyle: "solid",
-  borderColor: "white"
-}
+
 
 const Footer = () => {
   return (
     <StyleFooter>
-      <p>Our Team</p>
-
-      <div style={{ display: "flex", alignSelf: "center", justifyContent: "center", }}>
-        <Avatar sx={AvatarStyle} alt="Kok Sopanha" src={Panha} />
-        <Avatar sx={AvatarStyle} alt="Chen Darot" src={Darot} />
-        <Avatar sx={AvatarStyle} alt="Choem Thea" src={Thea} />
-        <Avatar sx={AvatarStyle} alt="Chomrouen Vatanak" src={Vatanak} />
-        <Avatar sx={AvatarStyle} alt="Kang Rotsarak" src={Sarak} />
-      </div>
+      <Typography sx={{padding: '20px 0',color: 'white',fontSize: '16px' }}>&copy;CADTG6 2022 - Made with ☕️ by <Link style={{color: 'white',fontSize: '16px'}} to='/team'>Team 4</Link></Typography>
     </StyleFooter>
   );
-};
+}
 
 export default Footer;
