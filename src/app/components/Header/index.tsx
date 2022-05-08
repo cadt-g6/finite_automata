15b470f38673cf8b6e0af33f7b37210b4c359155
Sopanha Kok <38602188+SopanhaKok@users.noreply.github.com>
2022-05-09 00:13:31 +0700

    Features/header (#25)
    * fix: update font loader from Montserrat to Roboto
    
    * fix: update Header design

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -2,18 +2,16 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Logo from '../Logo';
-import MenuLink from './MenuLink/MenuLink';
-import MenuDropDown from './MenuDropDown/MenuDropDown';
+import OurTeam from './OurTeam/OurTeam';
 
 interface Props {}
 
 const Header = (props: Props) => {
   return (
-    <AppBar position="static">
-      <Toolbar>
+    <AppBar sx={{backgroundColor: '#192849'}} position="static">
+      <Toolbar sx={{display: 'flex',justifyContent: 'space-between'}}>
         <Logo />
-        <MenuLink />
-        <MenuDropDown />
+        <OurTeam />
       </Toolbar>
     </AppBar>
   );
