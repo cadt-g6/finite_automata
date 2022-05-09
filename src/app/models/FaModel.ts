013e6a791b17a6fbe8202a5fe5133356a7606048
Vatanak Chamroeun <63393872+VatanakChamroeun@users.noreply.github.com>
2022-05-10 01:08:32 +0700

    feat: test if a string is accepted by a FA (#30)

--- src/app/models/FaModel.ts
+++ src/app/models/FaModel.ts
@@ -36,6 +36,101 @@ class FaModel {
     }
     return isNfa ? true : false;
   }
+
+  //test if string is accepted by DFA
+  stringAcceptedByDFA(targetString, transitions) {
+    let i = 0;
+    let nextTransition;
+    targetString.split('').forEach(symbol => {
+      if (i == 0) {
+        nextTransition = transitions[this.startState][symbol][0];
+        console.log(symbol, 'x', this.startState, '->', nextTransition);
+        i++;
+      } else {
+        nextTransition = transitions[nextTransition][symbol][0];
+        console.log(
+          symbol,
+          'x',
+          transitions[nextTransition][symbol][0],
+          '->',
+          nextTransition,
+        );
+        i++;
+      }
+    });
+
+    if (this.endStates.includes(nextTransition)) return true;
+    else return false;
+  }
+
+  //test if string is accepted by NFA
+  findAllStates = (
+    targetString,
+    transitions,
+    startStates = this.startState,
+    result: any = [],
+  ) => {
+    let state = startStates;
+
+    // Loop each character of target String
+    targetString.split('').every((input, indexString) => {
+      // Check if a state has transition with Epsilon
+      if (transitions[state]['E'] && transitions[state]['E'].length !== 0) {
+        transitions[state]['E'].forEach((transition, index) => {
+          this.findAllStates(
+            targetString.slice(indexString),
+            transitions,
+            transitions[state]['E'][index],
+            result,
+          );
+        });
+      }
+
+      // Check if there're more than 1 transition of a symbol
+      // Then Loop through each transition and Find Possible Outcome states
+      if (transitions[state][input].length > 1) {
+        transitions[state][input].forEach((transition, index) => {
+          this.findAllStates(
+            targetString.slice(indexString + 1),
+            transitions,
+            transitions[state][input][index],
+            result,
+          );
+        });
+
+        state = '';
+        return false; // break loop
+      } else if (transitions[state][input].length === 0) {
+        // If there's no transition, Stop !!
+        return false; // break loop
+      } else {
+        const nextState = transitions[state][input][0];
+        state = nextState;
+        return true; // continue
+      }
+    });
+
+    if (state) result.push(state);
+
+    return result;
+    // return back every final states
+  };
+
+  stringAcceptedByNFA = (targetString, transitions) => {
+    const possibleState = this.findAllStates(
+      targetString,
+      transitions,
+      this.startState,
+    );
+    console.log(possibleState);
+    const found = this.endStates.find(val => {
+      if (possibleState.includes(val)) {
+        return true;
+      }
+    });
+    if (found) return true;
+    else return false;
+  };
 }
 
 export default FaModel;
