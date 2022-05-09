2e66bbd2151788ab561763fc5ccbd7f259ad902d
SopanhaKok <sopanhakok@gmail.com>
2022-05-09 22:24:33 +0700

    fix: update Header styles

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -2,14 +2,22 @@ import React from 'react';
 import { styled } from '@mui/material';
 
 import IdtLogo from 'app/assets/IdtLogo.svg';
+import { Link } from 'react-router-dom';
 
-const Img = styled(`img`)({
+const Img = styled(`img`)(({ theme }) => ({
   height: '42px',
   marginRight: '36px',
-});
+  [theme.breakpoints.down('sm')]: {
+    height: '30px',
+  },
+}));
 
 const Logo = () => {
-  return <Img src={IdtLogo} />;
+  return (
+    <Link to="/">
+      <Img src={IdtLogo} />
+    </Link>
+  );
 };
 
 export default Logo;
