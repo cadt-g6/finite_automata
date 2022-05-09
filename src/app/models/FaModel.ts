bb290561f9f9887c5f4cd88c522292afc5c20cdc
Rotsarak <rotsarak.kang@gmail.com>
2022-05-09 17:03:18 +0700

    Feat: nfa to dfa

--- src/app/models/FaModel.ts
+++ src/app/models/FaModel.ts
@@ -36,6 +36,52 @@ class FaModel {
     }
     return isNfa ? true : false;
   }
+
+  nfatodfa(): any {
+    let nfa:any = JSON.parse(JSON.stringify(this.transitions));
+    let nfa_state:string[] = this.states.slice();
+    let dfa_state:string[] = this.states.slice();
+    let path:string[] = this.symbols.slice();
+
+    //generate all dfa state
+    nfa_state.forEach(function(i,indexi){
+      for(const j in nfa[i]){
+        let t = ''
+        nfa[i][j].forEach(function(k,indexk){
+          t=t+k
+        })
+        if (!dfa_state.includes(t) && t.toLowerCase() != 'null'){
+          dfa_state.push(t)
+        }
+      }
+    })
+    
+    //set transition to dfa
+    let dfa:any = {}
+    nfa_state.forEach(function(i,indexi){
+      dfa[i]={}
+      for(const j in nfa[i]){
+        let t = ''
+        nfa[i][j].forEach(function(k,indexk){
+          t=t+k
+        })
+        dfa[i][j]=t
+      }
+    })
+    
+    let dfa_state_n = dfa_state.filter(val => !nfa_state.includes(val));
+    
+    //set transition to new generated state
+    dfa_state_n.forEach(function(i,indexi){
+      dfa[i]={}
+      path.forEach(function(j,indexj){
+        dfa[i][j]=i
+      })
+    })
+    console.log("Final DFA",dfa)
+
+    return dfa;
+  }
 }
 
 export default FaModel;
