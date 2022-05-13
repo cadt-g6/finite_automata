3e81aee50df31515eb8298ea8aabb526cf25be10
SopanhaKok <sopanhakok@gmail.com>
2022-05-14 00:52:14 +0700

    update existing dialog and add new DfatoNfa dialog

--- src/app/components/Dialogs/DfatoNfaDialog.tsx
+++ src/app/components/Dialogs/DfatoNfaDialog.tsx
@@ -12,8 +12,11 @@ import {
   useTheme,
 } from '@mui/material';
 import { toStateString } from 'utils/string-utils';
+import FaModel from 'app/models/FaModel';
+import Graphviz from 'graphviz-react';
+import TransitionTable from 'app/pages/AddFaPage/components/TransitionTable';
 
-const SimpleDialog = ({
+const DfatoNfaDialog = ({
   content,
   open,
   handleClose,
@@ -26,25 +29,39 @@ const SimpleDialog = ({
   const states = toStateString('States', faData.states || []);
   const symbols = toStateString('Symbols', faData?.symbols || []);
   const finalStates = toStateString('Final states', faData?.endStates || []);
+  const newDfa = faData.nfatodfa();
+
+  const newStates = toStateString('States', newDfa.states || []);
+  const newSymbols = toStateString('Symbols', newDfa?.symbols || []);
+  const newFinalStates = toStateString('Final states', newDfa?.endStates || []);
   return (
     <BaseDialog open={open} onClose={handleClose}>
       <DialogTitle sx={{ fontSize: '16px', fontWeight: '400' }}>
-        Test if a FA is deterministic or non-deterministic
+        NFA to DFA
       </DialogTitle>
 
       <DialogContent>
-        <DialogContentText id="alert-dialog-description">
-          {content}
-        </DialogContentText>
         <Box sx={{ marginTop: '20px' }}>
           <Typography>{states}</Typography>
           <Typography>{symbols}</Typography>
           <Typography>{finalStates}</Typography>
-        </Box>
 
-        {graph}
-        {transitionTable}
+          {graph}
+          {transitionTable}
+        </Box>
+        <Typography variant="h6" sx={{ marginTop: '20px' }}>
+          After Convert
+        </Typography>
+        <Box sx={{ marginTop: '20px' }}>
+          <Typography>{newStates}</Typography>
+          <Typography>{newSymbols}</Typography>
+          <Typography>{newFinalStates}</Typography>
+
+          <Graphviz dot={newDfa.toDotString()} options={{ height: '200px' }} />
+          <TransitionTable faData={newDfa} />
+        </Box>
       </DialogContent>
+
       <DialogActions>
         <Button onClick={handleClose} autoFocus>
           CLose
@@ -54,4 +71,4 @@ const SimpleDialog = ({
   );
 };
 
-export default SimpleDialog;
+export default DfatoNfaDialog;
